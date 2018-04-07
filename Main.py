5e7702eff0669ceea3dc8bd8196037b03e1a4c1e
Elora Brenneman <brennema@mcmaster.ca>
2018-04-07 15:45:04 -0400

    Updated GUI.  Main.py file has some notes throughout with explanations for changes.  ProgramGeometry.py has also changed quite a bit as the GUI layout has changed slightly.

--- Main.py
+++ Main.py
@@ -20,7 +20,6 @@ class ExampleApp(QMainWindow, ProgramGeometry.Ui_MainWindow):
 		QApplication.setStyle(QStyleFactory.create("Cleanlooks"))
 		self.dirCurrent = os.getcwd()
 
-
 		# Define menu bar actions
 		self.actionQuit_Application.setShortcut("Ctrl+Q")
 		self.actionQuit_Application.setStatusTip('Leave the Application')
@@ -84,7 +83,7 @@ class ExampleApp(QMainWindow, ProgramGeometry.Ui_MainWindow):
 			"You have chosen: " + str(fileNameTrial) + " to process.  "\
 			"Is this correct?",
 			QMessageBox.Yes | QMessageBox.No)
-		self.progressTrial.setValue(0)    ############################# ADDED to reset when new file loaded
+		self.progressTrial.setValue(0) 
 
 		if trialSelect == QMessageBox.Yes:
 			trialName = fileNameTrial
@@ -109,12 +108,7 @@ class ExampleApp(QMainWindow, ProgramGeometry.Ui_MainWindow):
 				pass
 
 	def force_plate(self): 
-		############################################################################################
-		#I would think about setting these variables that are hard coded into another .py file 
-		#within the directory names variables.py or soemthing, and then import them. 
-		#This will allow you to quickly know where to look to alter the program for other labs etc. 
-		# I would do the same thing for the sensitivity matrix. I think I save sensitivity matrices as
-		#.csv in my other programs and then import them. But again, you should save them in a sperate .py. 
+		numpy.set_printoptions(precision=4, suppress=True) #just for now for troubleshooting
 		xPos = self.lineX
 		yPos = self.lineY
 		zPos = self.lineZ
@@ -122,37 +116,57 @@ class ExampleApp(QMainWindow, ProgramGeometry.Ui_MainWindow):
 		samplingRate = int(self.lineSampling.text())
 		if plateNum == 0:
 			self.forcePlateImage.setPixmap(QPixmap(str(os.path.join(self.dirCurrent,'Plates.png'))))
-			print(self.dirCurrent)
 		elif plateNum == 1: 
 			self.forcePlateImage.setPixmap(QPixmap(str(os.path.join(self.dirCurrent,'Plate1.png'))))
-			x = 0.5/1000
-			y = -0.5/1000
-			z = -41.4/1000
+			try: dataFrame = pd.read_excel(str(os.path.join(self.dirCurrent,'SensitivityMatrix-FP1.xlsx')), header=None, skiprows=6, skip_footer=4, usecols=range(9,12))
+			except:
+				errorBox = QMessageBox.critical(self, 'File Not Found', 'The origin information for force plate #' +
+					str(plateNum) + ' is not found.  This file does not exist, or may be corrupt.') #try/except for finding sensitivity matrix excel sheet
+			originPlate = numpy.asarray(dataFrame)
+			x = originPlate[0, 0]/1000
+			y = originPlate[0, 1]/1000
+			z = originPlate[0, 2]/1000
 			xPos.setText(QString(str(x)))
 			yPos.setText(QString(str(y)))
 			zPos.setText(QString(str(z)))
 		elif plateNum == 2:
 			self.forcePlateImage.setPixmap(QPixmap(str(os.path.join(self.dirCurrent,'Plate2.png'))))
-			x = -0.2/1000
-			y = 0.6/1000
-			z = -39.8/1000
+			try: dataFrame = pd.read_excel(str(os.path.join(self.dirCurrent,'SensitivityMatrix-FP2.xlsx')), header=None, skiprows=6, skip_footer=4, usecols=range(9,12))
+			except:
+				errorBox = QMessageBox.critical(self, 'File Not Found', 'The origin information for force plate #' +
+					str(plateNum) + ' is not found.  This file does not exist, or may be corrupt.') #same for plate 2
+			originPlate = numpy.asarray(dataFrame)
+			x = originPlate[0, 0]/1000
+			y = originPlate[0, 1]/1000
+			z = originPlate[0, 2]/1000
 			xPos.setText(QString(str(x)))
 			yPos.setText(QString(str(y)))
 			zPos.setText(QString(str(z)))
 
 	def process(self):
 		warnings.filterwarnings("ignore")
+		numpy.set_printoptions(precision=4, suppress=True) #just for now for troubleshooting
 		self.figure.clf() #clear any previous data in the results
-		self.lineMeanCOPx.setText(QString())
-		self.lineMeanCOPy.setText(QString())
-		self.lineSDCOPx.setText(QString())
-		self.lineSDCOPy.setText(QString())
-		self.lineCOVCOPx.setText(QString())
-		self.lineCOVCOPy.setText(QString())
-		self.lineMinCOPx.setText(QString())
-		self.lineMinCOPy.setText(QString())
-		self.lineMaxCOPx.setText(QString())
-		self.lineMaxCOPy.setText(QString())
+		self.lineMeanSpeedx.setText(QString())
+		self.lineMeanSpeedy.setText(QString())
+		self.lineDistancex.setText(QString())
+		self.lineDistancey.setText(QString())
+		self.lineMSEx.setText(QString())
+		self.lineMSEy.setText(QString())
+		self.line005x.setText(QString())
+		self.line005y.setText(QString())
+		self.line01x.setText(QString())
+		self.line01y.setText(QString())
+		self.line025x.setText(QString())
+		self.line025y.setText(QString())
+		self.line05x.setText(QString())
+		self.line05y.setText(QString())
+		self.line075x.setText(QString())
+		self.line075y.setText(QString())
+		self.line09x.setText(QString())
+		self.line09y.setText(QString())
+		self.line095x.setText(QString())
+		self.line095y.setText(QString())
 
 		fileNameTrial = self.lineTrialName.text()
 		if len(fileNameTrial) <= 1:
@@ -170,21 +184,24 @@ class ExampleApp(QMainWindow, ProgramGeometry.Ui_MainWindow):
 				QMessageBox.Ok)
 			return
 		elif plateNum == 1: 
-			sensitivityMatrix = numpy.array([[1.4783, -0.0058, 0.0063, 0.0004, 0.0001, 0], 
-				[0.0075, 1.4736, 0.0111, 0.0005, 0, 0], [0.0141, -0.0151, 5.8125, -0.0079, 0.0011, -0.0012],
-				[0.0020, -0.0024, 0.0016, 0.5868, -0.0014, 0.0002], [0.0029, -0.0013, 0.0057, -0.0002, 0.5875, 0.0003],
-				[0.0011, 0.0049, 0.0009, -0.0017, -0.0006, 0.2955]])
+			try: dataFrame = pd.read_excel(str(os.path.join(self.dirCurrent,'SensitivityMatrix-FP1.xlsx')), header=None, skiprows=5, usecols=range(1,7))
+			except:
+				errorBox = QMessageBox.critical(self, 'File Not Found', 'The sensitivity matrix for force plate #' +
+					str(plateNum) + ' is not found. This file does not exist, or may be corrupt.')
+			sensitivityMatrix = numpy.asarray(dataFrame)
 		elif plateNum == 2:
-			sensitivityMatrix = numpy.array([[1.4836, -0.0013, 0.0118, 0.0001, 0.0003, 0], 
-				[-0.0015, 1.4831, 0.0161, 0.0002, 0.0008, 0], [0.0161, 0.0097, 5.8133, 0.0051, 0.0002, 0.0003],
-				[0.0071, -0.0028, 0.0069, 0.5908, 0.0043, -0.0003], [0.0138, 0.0054, -0.0048, 0.0026, 0.5938, 0.0007],
-				[0.0011, 0.0063, 0.0003, -0.0030, 0.0011, 0.2978]]) 
-		
+			try: dataFrame = pd.read_excel(str(os.path.join(self.dirCurrent,'SensitivityMatrix-FP2.xlsx')), header=None, skiprows=5, usecols=range(1,7))
+			except:
+				errorBox = QMessageBox.critical(self, 'File Not Found', 'The sensitivity matrix for force plate #' +
+					str(plateNum) + ' is not found. This file does not exist, or may be corrupt.')
+			sensitivityMatrix = numpy.asarray(dataFrame)
+
 		try: analog_data = pd.read_csv(str(fileNameTrial), sep=',', skiprows=3, usecols=range(1,13))
 		except:
 			errorBox = QMessageBox.critical(self, 'File Error', 'You are procssing a file named: ' +
-				str(fileNameTrial) + '. This file is the wrong format. Check to ensure it ends with _Odau_1.csv')
+				str(fileNameTrial) + '. This file is in the wrong format.  Choose a file with the ending: _Odau_1.csv')
 			return None
+
 		if plateNum == 1: 
 			forcePlateAnalog = numpy.asarray(analog_data.loc[:,['Analog_1', 'Analog_2', 'Analog_3', 'Analog_4', 
 				'Analog_5', 'Analog_6',]])
@@ -193,36 +210,39 @@ class ExampleApp(QMainWindow, ProgramGeometry.Ui_MainWindow):
 				'Analog_11', 'Analog_12',]])
 
 		samplingRate = int(self.lineSampling.text())
+		samplingDuration = int(self.lineDuration.text()) #added trial duration in seconds
 		gain = int(self.lineGain.text())
 		vMax = int(self.lineVMax.text())
 		multiplier = float(self.lineMult.text())
 		Zo_m = float(self.lineZ.text())/1000
-		timeVel = numpy.linspace(0, 30, 29999)
+		timeVel = numpy.linspace(0, samplingDuration, ((samplingRate * samplingDuration)-1)) #instead of hard-coding for specific sampling rates, durations
 		forces_moments = numpy.matmul(sensitivityMatrix, forcePlateAnalog.T).T
 		forces_moments = forces_moments/(gain*vMax*multiplier)
+
+		FzChannel = forces_moments[:, 2] # Test for person on the force plate
+		bodyMass = FzChannel.mean()
+		if bodyMass < 300: #set really low for now; our test trials were actually collected at gain=2000 meaning calculated N values will be low
+			errorBox = QMessageBox.critical(self, 'Threshold Error', 'No force detected on the force plate.  Ensure ' +
+				'the correct force plate was chosen for processing.')
+			return 
+		else:
+			pass
+
 		CofPx = ((Zo_m * (forces_moments[:, 0])) - (forces_moments[:, 4])) / forces_moments[:, 2]
 		CofPy = ((Zo_m * (forces_moments[:, 1])) - (forces_moments[:, 3])) / forces_moments[:, 2]
-		meanCOPx = str(round(CofPx.mean(), 4))
-		meanCOPy = str(round(CofPy.mean(), 4))
-		stdCOPx = str(round(CofPx.std(), 4))
-		stdCOPy = str(round(CofPy.std(), 4))
-		covCOPx = str(round(CofPx.std()/CofPx.mean() * 100, 4))
-		covCOPy = str(round(CofPy.std()/CofPy.mean() * 100, 4))
-		minCOPx = str(round(CofPx.min(), 4))
-		minCOPy = str(round(CofPy.min(), 4))
-		maxCOPx = str(round(CofPx.max(), 4))
-		maxCOPy = str(round(CofPy.max(), 4))
-
 		centeredCofPx = CofPx - (CofPx.mean())
 		centeredCofPy = CofPy - (CofPy.mean())
-
 		velocity_x = numpy.diff(centeredCofPx) / (1./samplingRate)
 		velocity_y = numpy.diff(centeredCofPy) / (1./samplingRate)
 		velocity_resultant = numpy.sqrt(velocity_x**2 + velocity_y**2)
 		
-		framesPos = numpy.linspace(1, 30000, 30000)
-		framesVel = numpy.linspace(1, 29999, 29999)
+		framesPos = numpy.linspace(1, (samplingRate * samplingDuration), (samplingRate * samplingDuration)) #same as above
+		framesVel = numpy.linspace(1, (samplingRate * samplingDuration)-1, (samplingRate * samplingDuration)-1) #same as above
+		self.centredCofPx = numpy.vstack((framesPos, centeredCofPx))
+		self.centredCofPy = numpy.vstack((framesPos, centeredCofPy))
 		self.centredCofP = numpy.vstack((framesPos, centeredCofPx, centeredCofPy))
+		self.velocityx = numpy.vstack((framesVel, velocity_x))
+		self.velocityy = numpy.vstack((framesVel, velocity_y))
 		self.velocity = numpy.vstack((framesVel, velocity_x, velocity_y))
 
 		self.completed = 0
@@ -230,18 +250,30 @@ class ExampleApp(QMainWindow, ProgramGeometry.Ui_MainWindow):
 			self.completed += 0.0005
 			self.progressTrial.setValue(self.completed)
 
-		self.lineMeanCOPx.setText(QString(meanCOPx))
-		self.lineMeanCOPy.setText(QString(meanCOPy))
-		self.lineSDCOPx.setText(QString(stdCOPx))
-		self.lineSDCOPy.setText(QString(stdCOPy))
-		self.lineCOVCOPx.setText(QString(covCOPx))
-		self.lineCOVCOPy.setText(QString(covCOPy))
-		self.lineMinCOPx.setText(QString(minCOPx))
-		self.lineMinCOPy.setText(QString(minCOPy))
-		self.lineMaxCOPx.setText(QString(maxCOPx))
-		self.lineMaxCOPy.setText(QString(maxCOPy))
+		######### Populate these with the new variables
+		#self.lineMeanSpeedx.setText(QString())
+		#self.lineMeanSpeedy.setText(QString())
+		#self.lineDistancex.setText(QString())
+		#self.lineDistancey.setText(QString())
+		#self.lineMSEx.setText(QString())
+		#self.lineMSEy.setText(QString())
 
-		ax1 = self.figure.add_subplot(131)
+		#self.line005x.setText(QString())
+		#self.line005y.setText(QString())
+		#self.line01x.setText(QString())
+		#self.line01y.setText(QString())
+		#self.line025x.setText(QString())
+		#self.line025y.setText(QString())
+		#self.line05x.setText(QString())
+		#self.line05y.setText(QString())
+		#self.line075x.setText(QString())
+		#self.line075y.setText(QString())
+		#self.line09x.setText(QString())
+		#self.line09y.setText(QString())
+		#self.line095x.setText(QString())
+		#self.line095y.setText(QString())
+
+		ax1 = self.figure.add_subplot(131) #these subplots will change (centred COP, Vel., MSE)
 		ax1.plot(CofPx, CofPy, 'k')
 		ax1.set_title('CoPy vs CoPx (m)')
 		ax2 = self.figure.add_subplot(132)
@@ -250,25 +282,45 @@ class ExampleApp(QMainWindow, ProgramGeometry.Ui_MainWindow):
 		ax3 = self.figure.add_subplot(133) 
 		ax3.plot(timeVel, velocity_x, label="Vel x")
 		ax3.plot(timeVel, velocity_y, label="Vel y")
-		ax3.set_title('CoP Velocity (m/s) vs Time (s)')
+		ax3.set_title('Vel (m/s) vs Time (s)')
 		ax3.legend()
 		self.canvas.draw()
 
 	def save_results(self):
+		centredCofPx = self.centredCofPx #this way, only ML or AP can be saved... or both if both are clicked
+		centredCofPy = self.centredCofPy
 		centredCofP = self.centredCofP
+		velocityx = self.velocityx
+		velocityy = self.velocityy
 		velocity = self.velocity
-		if self.checkBoxCOPPos.isChecked():
+		if self.checkBoxCOPPos.isChecked() and self.checkBoxCOPPosy.isChecked():
 			datafr = pd.DataFrame(centredCofP.T, columns=['Frames', 'COPx', 'COPy'])
 			filepath = str(self.lineTrialName.text()) + '_COP_Position.xlsx'
 			datafr.to_excel(filepath, index=False)
-		elif not self.checkBoxCOPPos.isChecked():
+		elif self.checkBoxCOPPos.isChecked():
+			datafr = pd.DataFrame(centredCofPx.T, columns=['Frames', 'COPx'])
+			filepath = str(self.lineTrialName.text()) + '_COP_Positionx.xlsx'
+			datafr.to_excel(filepath, index=False)
+		elif self.checkBoxCOPPosy.isChecked():
+			datafr = pd.DataFrame(centredCofPy.T, columns=['Frames', 'COPy'])
+			filepath = str(self.lineTrialName.text()) + '_COP_Positiony.xlsx'
+			datafr.to_excel(filepath, index=False)
+		elif not self.checkBoxCOPPos.isChecked() and self.checkBoxCOPPosy.isChecked():
 			pass
 
-		if self.checkBoxCOPVel.isChecked():
+		if self.checkBoxCOPVel.isChecked() and self.checkBoxCOPVely.isChecked():
 			datafra = pd.DataFrame(velocity.T, columns=['Frames', 'COPx Vel.', 'COPy Vel.'])
 			filepaths = str(self.lineTrialName.text()) + '_COP_Velocity.xlsx'
 			datafra.to_excel(filepaths, index=False)
-		elif not self.checkBoxCOPVel.isChecked():
+		elif self.checkBoxCOPVel.isChecked():
+			datafra = pd.DataFrame(velocityx.T, columns=['Frames', 'COPx Vel.'])
+			filepaths = str(self.lineTrialName.text()) + '_COP_Velocityx.xlsx'
+			datafra.to_excel(filepaths, index=False)
+		elif self.checkBoxCOPVely.isChecked():
+			datafra = pd.DataFrame(velocityy.T, columns=['Frames', 'COPy Vel.'])
+			filepaths = str(self.lineTrialName.text()) + '_COP_Velocityy.xlsx'
+			datafra.to_excel(filepaths, index=False)
+		elif not self.checkBoxCOPVel.isChecked() and self.checkBoxCOPVely.isChecked():
 			pass
 
 
